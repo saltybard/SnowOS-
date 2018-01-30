f24ccb54c60147d95766c7567e68054c7e49e14c
Cassie Renz <cbr.work@hotmail.comgit config --global user.email cbr.work@hotmail.com>
2018-01-30 15:46:12 -0800

    working with exec

--- HW1/hw1.c
+++ HW1/hw1.c
@@ -3,24 +3,36 @@
 #include <unistd.h>
 
 int main() {
-	char cmd1[255], cmd2[255], cmd3[255];
-	//change to 256, check for a '\n' and if there isn't one then there might be an issue
+	char cmd1[255], cmd2[255], cmd3[255], filename[255];
+	//don't need to worry about buffer overflow :> 
+
+	char * cmd = "wc";
+	char * cmdTest[3];
+	cmdTest[0] = "ls";
+	cmdTest[1] = "-a";
+	cmdTest[2] = NULL; 
 
 	//read in 3 strings from the console
-	// Error handling: no more than 255 characters, no more than 5 total strings when split up 
+	 
 	printf("mash-1>");
 	fgets(cmd1, sizeof(cmd1), stdin);
-	while((getchar()) != '\n');
-        //scanf("%s", cmd1);
+	
 	printf("mash-2>");
 	fgets(cmd2, sizeof(cmd2), stdin);
-	while((getchar()) != '\n');
+	
 	printf("mash-3>");
 	fgets(cmd3, sizeof(cmd3), stdin);
-	while((getchar()) != '\n');
+
+	printf("filename>");
+	fgets(filename, sizeof(filename), stdin);
+
+	
+	
 	printf("cmd 1: %s", cmd1);
 	printf("cmd 2: %s", cmd2);
 	printf("cmd 3: %s", cmd3);
+
+	execvp(cmdTest[0], cmdTest);
 
 
 
