29b85acf467329f40dbbb3bb3b4cb031a07cd0d1
Cassie Renz <cbr.work@hotmail.comgit config --global user.email cbr.work@hotmail.com>
2018-01-30 18:41:07 -0800

    Parsing working, need to put it in a function atm

--- HW1/hw1.c
+++ HW1/hw1.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 int main() {
 	char cmd1[255], cmd2[255], cmd3[255], filename[255];
+	char *token, *newline;
+	char* command1[7], command2[7], command3[7];
 	//don't need to worry about buffer overflow :> 
-
-	char * cmd = "wc";
-	char * cmdTest[3];
-	cmdTest[0] = "ls";
-	cmdTest[1] = "-a";
-	cmdTest[2] = NULL; 
 
 	//read in 3 strings from the console
 	 
@@ -27,12 +24,37 @@ int main() {
 	fgets(filename, sizeof(filename), stdin);
 
 	
-	
+	char file[255] = "test.txt";	
+
+	//each array needs to be pointing to an array of strings, which end w/ the file name then w/ null
+
 	printf("cmd 1: %s", cmd1);
 	printf("cmd 2: %s", cmd2);
 	printf("cmd 3: %s", cmd3);
+	printf("filename: %s", filename);
+	
 
-	execvp(cmdTest[0], cmdTest);
+	//iterate over char array
+
+	int i = 0; 
+	token = strtok(cmd1, " ");
+
+	while(token != NULL) { 
+		command1[i] = token;
+		i = i + 1; 
+		token = strtok(NULL, " "); 
+		token = strtok(token, "\n");
+	} 
+
+	command1[i--] = filename;
+
+	printf("%s\n", command1[0]);
+	printf("%s\n", command1[1]);
+	printf("%s\n", command1[2]);
+	//printf("%s\n", command1[i]);
+
+	//printf("-----LAUNCH CMD 1: %s ----------------------------------------\n", cmdTest[0]);
+	//execvp(command you want to execute, array w/ whole command + filename at end if applicable);
 
 
 
