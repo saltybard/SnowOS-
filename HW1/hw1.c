a76cc25cddc7b9f6a45f319bbbbbabc0a0ae2879
Exeter <stephanie_day8@msn.com>
2018-02-02 16:14:10 -0800

    As far as I know, parallel forks are done.

--- HW1/hw1.c
+++ HW1/hw1.c
@@ -2,8 +2,11 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 char **parseCommand(char *cmd, char *filename);
+void forkIt(char** cmd1, char** cmd2, char** cmd3);
 
 
 
@@ -36,18 +39,18 @@ int main() {
 	char ** command2 = parseCommand(cmd2, filename);
 	char ** command3 = parseCommand(cmd3, filename); 
 
-
-	printf("cmd 1: %s\n", command1[0]);
-	printf("cmd 2: %s\n", command2[0]);
-	printf("cmd 3: %s\n", command3[0]);
-	printf("filename: %s\n", filename);	
+	forkIt(command1, command2, command3);
+	/* printf("cmd 1: %s\n", command1[0]); */
+	/* printf("cmd 2: %s\n", command2[0]); */
+	/* printf("cmd 3: %s\n", command3[0]); */
+	/* printf("filename: %s\n", filename); */	
 	
 
-	execvp(command1[0], command1); 
+	 
 
 	//printf("-----LAUNCH CMD 1: %s ----------------------------------------\n", cmdTest[0]);
 	//execvp(command you want to execute, array w/ whole command + filename at end if applicable);
-
+	
 
 }
 
@@ -65,15 +68,45 @@ char ** parseCommand (char *cmd, char *filename) {
 
 	int i = 0; 
 	token = strtok(cmd, " \n");
-	printf("First token grabbed: %s\n", token);
+	//printf("First token grabbed: %s\n", token);
 	while(token != NULL) { 
 		newCmd[i] = token;
 		//printf("Does my array work: %s\n", newCmd[i]);
 		i++; 
 		token = strtok(NULL, " \n");
-		printf("Token grabbed: %s\n", token);
+		//printf("Token grabbed: %s\n", token);
 	} 
         newCmd[i] = filename;
 	
 	return newCmd;
+}
+
+void forkIt(char** cmd1, char** cmd2, char** cmd3) {
+  int childIds[3];
+  int p1 = fork();
+  if (p1 == 0) { // first child
+    execvp(cmd1[0], cmd1);//first child's command HERE
+  }
+  if (p1 > 0) { // back in the parent
+    childIds[0] = p1;
+    int p2 = fork();
+    if (p2 == 0) {//second child
+      execvp(cmd2[0], cmd2);//second child's command HERE
+    }
+    if (p2 > 0) { // back in p1
+      childIds[1] = p2;
+      int p3 = fork();
+      if (p3 == 0) {
+	execvp(cmd3[0], cmd3);//third child's command HERE
+      }
+      if (p3 > 0) {
+	childIds[2] = p3;
+	wait(NULL);
+      }
+      wait(NULL);
+    }
+    wait(NULL);
+  }
+  printf("Children: %d, %d, %d\n", childIds[0], childIds[1], childIds[2]);
+  printf("exiting..\n");
 }
