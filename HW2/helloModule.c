d8721917985892738cf0068892439b829392ad1a
Exeter <stephanie_day8@msn.com>
2018-02-19 12:33:29 -0800

    Organizing before merge.

--- HW2/helloModule.c
+++ HW2/helloModule.c
@@ -1,7 +1,15 @@
 #include<linux/module.h>
+#include<linux/kernel.h>
+#include<linux/printk.h>
+#include<linux/sched.h>
 
 int proc_init (void) {
+  struct task_struct *task;
   printk(KERN_INFO "helloModule: kernel module initialized\n");
+
+  for_each_process(task) {
+    printk("%s[%d]\n", task->comm, task->pid);
+  }
   return 0;
 }
 
