71181088173da6ce62b21c1d3d9b0d252646cfaa
Exeter <stephanie_day8@msn.com>
2018-02-21 10:13:41 -0800

    Some traversing loops done

--- HW2/helloModule.c
+++ HW2/helloModule.c
@@ -4,12 +4,25 @@
 #include<linux/sched.h>
 
 int proc_init (void) {
-  struct task_struct *task;
+  struct task_struct* task;
+  struct task_struct* child;
+  struct list_head* childList;
   printk(KERN_INFO "helloModule: kernel module initialized\n");
-
+  int runnable = 0;
+  int unrunnable = 0;
+  int stopped = 0;
   for_each_process(task) {
     printk("%s[%d]\n", task->comm, task->pid);
+    if (task->state == -1) unrunnable++;
+    if (task->state == 0) runnable++;
+    if (task->state > 0) stopped++;
+    list_for_each(childList, &task->children) {
+      child = list_entry(childList, struct task_struct, sibling);
+      printk("Child: %s", child->comm);
+    }
   }
+  
+  printk("Process States: %d -- %d -- %d\n", runnable, unrunnable, stopped);
   return 0;
 }
 
